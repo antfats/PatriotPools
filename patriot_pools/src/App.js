56caa45c9a740a79cf56fac93f993ff9c8340cb5
antfats <antfats@hotmail.com>
2019-07-21 14:34:04 -0700

    inital commit

---
+++ patriot_pools/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="ui labeled button" tabindex="0">
+      <div className="ui basic blue button">
+        <i className="fork icon"></i> Forks
+    </div>
+      <a className="ui basic left pointing blue label">
+        1,048
+    </a>
+    </div>
+  );
+}
+
+export default App;
